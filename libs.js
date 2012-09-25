87dace2d5f789eec7b78d134ca99393fb286f989
Brian Cavalier <brian@hovercraftstudios.com>
2012-09-25 14:29:50 -0400

    move paranoid setting

--- libs.js
+++ libs.js
@@ -1,3 +1,17 @@
+// This turns off Object.freeze() in when.js
+// The other libs (except Q) *do not* use Object.freeze(),
+// and there is currently a bug (imho) in v8 that causes
+// large performance degredations on frozen objects:
+
+// http://stackoverflow.com/questions/8435080/any-performance-benefit-to-locking-down-javascript-objects
+
+// So, this levels the test playing field for all libs
+// except Q, which as far as I can tell, has no way of
+// disabling its calls to Object.freeze()
+
+process.env.WHEN_PARANOID = false;
+
+// Load all promise impls
 module.exports = {
 	when: require('when'),
 	q: require('q'),
--- run.sh
+++ run.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 
+# Simple test runner and separator
 function runTest {
 	echo "---------------------------"
 	echo "Test: $1"
 	echo "---------------------------"
 	node $1
 }
-
-# This turns off Object.freeze() in when.js
-# The other libs (except Q) *do not* use Object.freeze(),
-# and there is currently a bug (imho) in v8 that causes
-# large performance degredations on frozen objects:
-# 
-# http://stackoverflow.com/questions/8435080/any-performance-benefit-to-locking-down-javascript-objects
-# 
-# So, this levels the test playing field for all libs
-# except Q, which as far as I can tell, has no way of
-# disabling its calls to Object.freeze()
-export WHEN_PARANOID=false
 
 # Promise basics
 runTest promise-create.js
